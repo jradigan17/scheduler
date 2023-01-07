13fcf1060646875e86fbfbdb96aa4c8269bda726
jradigan17 <jradigan@hotmail.com>
2023-01-07 03:12:50 -0500

    button function

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,26 @@
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+
+   // let buttonClass = 'button';
+   // let disabledButton = '';
+
+   // if (props.danger) {
+   //    buttonClass += ' button--danger'
+   // } else if (props.confirm) {
+   //    buttonClass += ' button--confirm'
+   // }
+
+   const buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+   });
+   
+   return (
+   <button className={`${buttonClass}`} disabled={props.disabled} onClick={props.onClick}>
+      {props.children}
+   </button>
+   );
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+// -------------------------------------
+// Storybook - npm run storybook - Localhost: 8000
+// Webpack Dev Server - npm start - Localhost: 9009
+// Jest: test runner - npm test (a to run all tests)
+// Cypress: End-to-End Testing
+
+
 import React from "react";
 import ReactDOM from "react-dom";
 
